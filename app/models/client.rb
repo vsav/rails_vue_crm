e6af26984736d19df772e50e15edfdab738b9580
Vadim Savostin <vsavdev@gmail.com>
2020-09-15 03:14:33 +0300

    move common code to concern

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,4 @@
 class Client < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  include DeviseAuthenticatable
+
 end
