46b925357ef028a8c2803731758f741d39dc6806
Vadim Savostin <vsavdev@gmail.com>
2020-09-25 15:04:47 +0300

    add clients_list and create_client methods

--- app/controllers/staffs/home_controller.rb
+++ app/controllers/staffs/home_controller.rb
@@ -4,7 +4,31 @@ class Staffs::HomeController < ApplicationController
   def index; end
 
   def profile
-    profile = StaffSerializer.new(current_staff).serializable_hash
-    render json: profile[:data][:attributes]
+    render json: current_staff, serializer: StaffSerializer
+  end
+
+  def clients_list
+    clients = Client.all
+    render json: clients, each_serializer: ClientSerializer
+  end
+
+  def create_client
+    @client = Client.new(client_params)
+
+    if @client.save
+      render json: @client, serializer: ClientSerializer, status: :created
+    else
+      render json: { errors: @client.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:email,
+                                   :full_name,
+                                   :phone,
+                                   :password,
+                                   :password_confirmation)
   end
 end
