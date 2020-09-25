47dd7b648d9ee26d5350d71f8e84c0c12b04ecdf
Vadim Savostin <vsavdev@gmail.com>
2020-09-25 15:07:17 +0300

    add full_name and phone to clients, validations, seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_225005) do
+ActiveRecord::Schema.define(version: 2020_09_23_085828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2020_09_18_225005) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "api_token", default: -> { "gen_random_uuid()" }
+    t.string "phone", null: false
+    t.string "full_name", null: false
     t.index ["api_token"], name: "index_clients_on_api_token", unique: true
     t.index ["email"], name: "index_clients_on_email", unique: true
     t.index ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
