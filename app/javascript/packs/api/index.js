f52a787391b576282fbf900dcc136da95dc7d8dc
Vadim Savostin <vsavdev@gmail.com>
2020-09-25 15:08:07 +0300

    create index.js with axios helpers

--- app/javascript/packs/api/index.js
+++ app/javascript/packs/api/index.js
@@ -2,7 +2,11 @@ import axios from 'axios'
 import Vue from 'vue'
 
 const api = {
-  profile: () => axios.get('/staffs/profile')
+  profile: () => axios.get('/staffs/profile'),
+  clients: {
+    index: () => axios.get('/staffs/clients_list'),
+    create: (client) => axios.post('/staffs/create_client', client)
+  }
 }
 
 Vue.prototype.$api = api
