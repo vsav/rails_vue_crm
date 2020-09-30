b5ef6e167c01cf1e4d6d99d66ca443bee4712aa2
Vadim Savostin <vsavdev@gmail.com>
2020-09-30 23:55:24 +0300

    add quasar with material iconset

---
+++ app/javascript/packs/quasar/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Quasar from 'quasar'
+import 'quasar/dist/quasar.sass'
+import iconSet from 'quasar/icon-set/svg-material-icons'
+import '@quasar/extras/material-icons/material-icons.css'
+
+Vue.use(Quasar, {
+  iconSet: iconSet
+})
