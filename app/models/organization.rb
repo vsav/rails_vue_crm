0377070ba2c0578341527aa498eee2c6a9f76acb
Vadim Savostin <vsavdev@gmail.com>
2020-10-07 02:21:51 +0300

    add uniqueness validation

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,4 +3,5 @@ class Organization < ApplicationRecord
   has_many :clients, through: :client_organizations
 
   validates :name, :structure, :inn, :ogrn, presence: true
+  validates_uniqueness_of :name, :inn, :ogrn
 end
