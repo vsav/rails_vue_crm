564468e1b38bf35d48753d1247eb5e485dd6c5f5
Vadim Savostin <vsavdev@gmail.com>
2020-10-06 16:20:53 +0300

    update client app with new components

--- app/javascript/client.vue
+++ app/javascript/client.vue
@@ -1,21 +1,30 @@
 <template lang="pug">
-  #app
-    p {{ message }}
-    a(href="/clients/sign_out" data-method="delete") Sign out
+  q-layout(view="hHh lpR fFf")
+    template(v-if="loading")
+      Loading
+    template(v-else)
+      Navbar
+      ClientOrganizationsTable
 </template>
 
 <script>
-  export default {
-    data: function () {
-      return {
-        message: "Hello Client!"
-      }
-    }
-  }
-</script>
+import Navbar from './packs/shared/components/navbar'
+import Loading from './packs/shared/components/loading'
+import ClientOrganizationsTable from './client/components/organizationstable'
 
-<style lang="sass">
-  p
-    font-size: 2em
-    text-align: center
-</style>
+export default {
+  data: function() {
+    return {
+      loading: true
+    }
+  },
+  components: {
+    Navbar,
+    Loading,
+    ClientOrganizationsTable
+  },
+  created() {
+    this.loading = false
+  }
+}
+</script>
