e6ec881076c2291df844eecc3e46232f7cc39abf
Vadim Savostin <vsavdev@gmail.com>
2020-10-06 16:19:34 +0300

    create client organizations table component

---
+++ app/javascript/client/components/organizationstable.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+  q-page-container
+    q-table(
+      title="Organizations List"
+      :data="clientOrganizations"
+      :columns="columns"
+      :pagination.sync="pagination"
+      row-key="id"
+    )
+
+</template>
+<script>
+export default {
+  name: 'ClientOrganizationsTable',
+  data() {
+    return {
+      pagination: {
+        rowsPerPage: 10
+      },
+      errors: {},
+      clientOrganizations: [],
+      columns: [
+        {
+          name: 'name',
+          label: 'Name',
+          field: 'name',
+          align: 'left',
+          sortable: true
+        },
+        {
+          name: 'structure',
+          label: 'Structure',
+          field: 'structure',
+          align: 'left',
+          sortable: true
+        },
+        {
+          name: 'inn',
+          label: 'INN',
+          field: 'inn',
+          align: 'left',
+          sortable: true
+        },
+        {
+          name: 'ogrn',
+          label: 'OGRN',
+          field: 'ogrn',
+          align: 'left',
+          sortable: true
+        }
+      ]
+    }
+  },
+  created() {
+    this.fetchOrganizations()
+  },
+  methods: {
+    fetchOrganizations() {
+      this.$api.organizations.index()
+          .then(({data}) => this.clientOrganizations = data)
+          .catch((error) => this.errors['fetch'] = error)
+    }
+  }
+}
+</script>
