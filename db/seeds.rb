58d760a40d1d61540247b8ca374e18a4bc2f00c3
Vadim Savostin <vsavdev@gmail.com>
2020-10-07 02:28:35 +0300

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-clients = Client.create([
+clients = Client.create!([
                             {
                               email: 'john@test.com',
                               password: 'qwe123',
@@ -26,4 +26,82 @@ clients = Client.create([
                               password_confirmation: 'qwe123',
                               full_name: 'Kyle Reese',
                               phone: '1132423'
+                            },
+                            {
+                              email: 'max@test.com',
+                              password: 'qwe123',
+                              password_confirmation: 'qwe123',
+                              full_name: 'Max Cavalera',
+                              phone: '123213'
+                            },
+                            {
+                              email: 'igor@test.com',
+                              password: 'qwe123',
+                              password_confirmation: 'qwe123',
+                              full_name: 'Igor Cavalera',
+                              phone: '185924'
+                            },
+                            {
+                              email: 'macclein@test.com',
+                              password: 'qwe123',
+                              password_confirmation: 'qwe123',
+                              full_name: 'John Macclein',
+                              phone: '1432343'
                             }])
+Staff.create!(email: 'admin@staff.com', password: 'qwe123', password_confirmation: 'qwe123')
+
+organizations = Organization.create!([
+                            {
+                              name: 'Sepultura',
+                              structure: 'OOO',
+                              inn: '123123123',
+                              ogrn: '13345234'
+                            },
+                            {
+                              name: 'Soulfly',
+                              structure: 'ZAO',
+                              inn: '12574833',
+                              ogrn: '13547374'
+                            },
+                            {
+                              name: 'Terminator',
+                              structure: 'OAO',
+                              inn: '345234234',
+                              ogrn: '672348973'
+                            },
+                            {
+                              name: 'Terminator 2',
+                              structure: 'OAO',
+                              inn: '7483623423',
+                              ogrn: '462876423'
+                            },
+                            {
+                              name: 'Die Hard',
+                              structure: 'ZAO',
+                              inn: '847349534',
+                              ogrn: '456387234'
+                            },
+                            {
+                              name: 'Die Hard 2',
+                              structure: 'ZAO',
+                              inn: '812319534',
+                              ogrn: '47645234'
+                            },
+                            {
+                              name: 'Die Hard 3',
+                              structure: 'ZAO',
+                              inn: '8486428934',
+                              ogrn: '446283784'
+                            },
+                            {
+                              name: 'Die Hard 4',
+                              structure: 'ZAO',
+                              inn: '898234423',
+                              ogrn: '456482344'
+                            },])
+clients[0].organizations.push(organizations[2], organizations[3])
+clients[1].organizations.push(organizations[2], organizations[3])
+clients[2].organizations.push(organizations[2])
+clients[3].organizations.push(organizations[0], organizations[1])
+clients[4].organizations.push(organizations[0])
+clients[5].organizations.push(organizations[4], organizations[5], organizations[6], organizations[7])
