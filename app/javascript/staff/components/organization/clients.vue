86958aed3ba708df6532b0e676b41e6a4a69bbeb
Vadim Savostin <vsavdev@gmail.com>
2020-10-10 08:54:57 +0300

    create manage clients dialog table

---
+++ app/javascript/staff/components/organization/clients.vue
@@ -0,0 +1,93 @@
+<template lang="pug">
+  q-dialog(ref="organizationClientsDialog" persistent )
+    q-card(style="width: 700px; max-width: 80vw;")
+      q-card-section
+        q-table(
+          :title="organization.name + ' Clients List'"
+          :data="clients"
+          :columns="columns"
+          :pagination.sync="pagination"
+          selection="multiple"
+          :selected.sync="selected"
+          row-key="id"
+        )
+        q-btn(@click.prevent="updateClientsList(organization)" label="Update" color="orange" icon="warning" class="q-ma-md")
+        q-btn(@click.prevent="hide()" label="Cancel" class="q-ma-md")
+
+</template>
+<script>
+export default {
+  name: 'OrganizationClientsTable',
+  props: {
+    organization: Object
+  },
+  data() {
+    return {
+      pagination: {
+        rowsPerPage: 10
+      },
+      errors: {},
+      clients: [],
+      selected: [],
+      columns: [
+        {
+          name: 'full_name',
+          label: 'Full name',
+          field: 'full_name',
+          align: 'left',
+          sortable: true
+        },
+        {
+          name: 'email',
+          label: 'Email',
+          field: 'email',
+          align: 'left',
+          sortable: true
+        },
+        {
+          name: 'phone',
+          label: 'Phone',
+          field: 'phone',
+          align: 'left',
+          sortable: true
+        }
+      ]
+    }
+  },
+  created() {
+    this.fetchClients()
+  },
+  updated() {
+    this.organization.clients = this.selected
+  },
+  methods: {
+    fetchClients() {
+      this.$api.clients.index()
+          .then(({data}) => this.clients = data)
+          .catch((error) => this.errors['fetch'] = error)
+      this.selectClients()
+    },
+    updateClientsList(organization) {
+      this.loading = true
+      this.$api.organizations.update(organization)
+          .catch((error) => this.errors['update'] = error)
+          .finally(() => {
+            this.loading = false
+            this.hide()
+          })
+    },
+    show () {
+      this.$refs.organizationClientsDialog.show()
+    },
+    hide () {
+      this.$refs.organizationClientsDialog.hide()
+    },
+    selectClients() {
+      this.organization.clients.forEach(client => {
+        this.selected.push(client)
+      })
+    }
+  }
+}
+</script>
+
