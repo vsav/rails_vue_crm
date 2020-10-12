8671b0dbc0733114ed0da3821e4a1a3dae3f04e0
Vadim Savostin <vsavdev@gmail.com>
2020-10-12 09:03:12 +0300

    add edit client organizations ability

--- app/javascript/staff/components/client/table.vue
+++ app/javascript/staff/components/client/table.vue
@@ -9,11 +9,16 @@
     template(v-slot:body-cell-actions="client")
       q-td(:client="client")
         q-btn(@click.prevent="deleteClient(client)" icon="delete" round)
+        q-btn(@click="editClient(client)" icon="edit" round v-model="client")
+    template(v-slot:body-cell-organizations="client")
+      q-td(:client="client")
+        q-btn(@click="manageOrganizations(client)" icon="domain_disabled" round)
     template(v-slot:top-right)
       q-btn(label="Create Client" color="primary" @click="showForm" class="q-ma-md right" )
 </template>
 <script>
   import ClientForm from "./form";
+  import ClientOrganizationsTable from "./organizations"
 
   export default {
     name: 'ClientsTable',
@@ -24,6 +29,7 @@
         },
         errors: {},
         clients: [],
+        client: {},
         columns: [
           {
             name: 'full_name',
@@ -47,9 +53,14 @@
             sortable: true
           },
           {
+            name: 'organizations',
+            label: 'Organizations',
+            align: 'left'
+          },
+          {
             name: 'actions',
             label: 'Actions',
-            align: 'left',
+            align: 'left'
           }
         ]
       }
@@ -69,10 +80,29 @@
             .then(() => this.fetchClients())
             .catch((error) => this.errors['delete'] = error)
       },
+      setClient(client) {
+        this.client = client
+        this.client.organizations = client.organizations
+      },
       showForm() {
         this.$q.dialog({
           component: ClientForm,
           parent: this
+        })
+      },
+      manageOrganizations(client) {
+        this.$q.dialog({
+          component: ClientOrganizationsTable,
+          parent: this,
+          client: client.row
+        })
+      },
+      editClient(client) {
+        this.setClient(client.row)
+        this.$q.dialog({
+          component: ClientForm,
+          parent: this,
+          edited_client: this.client
         })
       }
     }
