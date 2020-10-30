f9263c239a3ea0853b9f21450c7b0b6b2ed09e24
Vadim Savostin <vsavdev@gmail.com>
2020-10-30 08:53:38 +0300

    add equipment tab

--- app/javascript/staff/components/dashboard.vue
+++ app/javascript/staff/components/dashboard.vue
@@ -3,10 +3,13 @@
     q-tabs(dense v-model="tab")
       q-route-tab(name="clients" label="Clients" icon="people_outlined" to="/clients")
       q-route-tab(name="organizations" label="Organizations"  icon="domain" to="/organizations")
+      q-route-tab(name="equipment" label="Equipment"  icon="biotech" to="/equipment_list")
     q-tab-panels(v-model="tab" animated)
       q-tab-panel(name="clients")
         router-view
       q-tab-panel(name="organizations")
+        router-view
+      q-tab-panel(name="equipment")
         router-view
 </template>
 
