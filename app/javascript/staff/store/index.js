75c72fa36016b67972ec0dc09d3b19f54b9d40db
Vadim Savostin <vsavdev@gmail.com>
2020-11-13 19:49:00 +0300

    create vuex store index

---
+++ app/javascript/staff/store/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import organizations from './modules/organizations';
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    organizations,
+  },
+  plugins: [createPersistedState()]
+})
