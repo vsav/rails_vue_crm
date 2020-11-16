3814f541d8d144775358562be84b08bf7d45fa8d
Vadim Savostin <vsavdev@gmail.com>
2020-11-16 13:52:57 +0300

    install and configure eslint

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,7 +3,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 const pugLoader = require('./loaders/pug')
 const sassLoader = require('./loaders/sass')
+const eslint = require('./loaders/eslint')
 
+environment.loaders.prepend('eslint', eslint)
 environment.loaders.prepend('pug', pugLoader)
 environment.loaders.prepend('sass', sassLoader)
 module.exports = environment.toWebpackConfig()
